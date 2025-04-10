c8b903c598a763ec098e912fa1170338aad678c4
Rensytimsy <rensytimsy03@gmail.com>
2025-04-10 22:26:48 +0300

    Socket initialization

--- app/api/socket-io/route.ts
+++ app/api/socket-io/route.ts
@@ -1,16 +1,18 @@
-import {Server as IOServer} from "socket.io"
+import {Server as IOServer} from "socket.io";
 import {Server as HTTPServer} from "http";
-
-import type {NextApiResponseWithSocket} from "@/types/socket"
 import type { NextApiRequest } from "next";
+import type { NextApiResponseWithSocket } from "@/types/socket";
 
-export const SocketIOHandler = (req:NextApiRequest, res:NextApiResponseWithSocket) => {
+export const SocketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
     if(!res.socket.server.io){
-        console.log("Initializing socket connection");
-
         const httpServer: HTTPServer = res.socket.server.io as any;
+        //Below is an instance of socket.io initialized if it was not existing
         const io = new IOServer(httpServer, {
-            path: "/api/socket-io"
-        })
+            path: " "
+        });
+
+        res.socket.server.io = io
+
     }
+    res.end();
 }
--- app/pages/chat-page/page.tsx
+++ app/pages/chat-page/page.tsx
@@ -1,8 +1,27 @@
 "use client"
 import { useEffect } from "react"
+import { SocketHandler } from "@/app/api/socket-io/route"
+import { io } from "socket.io-client";
 
 export default function ChatPage(){
 
+    let socket;
+    useEffect(() => {
+        SocketInitializer();
+    },[]);
+
+    const SocketInitializer = async() => {
+
+        await fetch("/api/socket-io");
+        
+        socket = io({
+            path: "/api/socket-io"
+        });
+
+        socket.on("connection", (socket) => {
+            console.log("Socket connected")
+        })
+    }
 
     return (
         <div>
